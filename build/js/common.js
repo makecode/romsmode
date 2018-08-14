8ac0a3a436aea1059bddde43747dd4693e3d5d99
Rita Akhmadulina <rita.akhmadulina@gmail.com>
2018-08-14 17:41:15 +0300

    add new page

--- build/js/common.js
+++ build/js/common.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
 
+  const popupOverlay = document.getElementById('popup-overlay');
+  const popup = document.getElementById('popup');
+  const btnPopup = document.getElementById('popup__close');
+
   (function RomsMode() {
 
     (function menuToggling() {
-      $('.js-menu-toggle').click(function(){
-        $('.mobile-menu').toggleClass('is-open');
-        $('.header__left').toggleClass('is-menu-open');
+      $('#js-menu-toggle').on('click',function() {
+        $(this).toggleClass('open');
+        $('.mobile-menu').toggleClass('is-open')
       });
     }());
 
@@ -39,7 +43,91 @@ $(document).ready(function() {
       });
     });
 
-    $('.search__action').click(search);
+    // $('body').onmousemove(function(event) {
+    //   elem.innerHTML = event.pageX + ' : ' + event.pageY;
+    //   if(event.pageX <= 0 ) {
+    //     $(popup).fadeIn('slow');
+    //     $(popupOverlay).fadeIn('slow');
+    //   }
+    // });
+    //
+    // const delayPopup = 10000;
+    //
+    // setTimeout(showPopup, delayPopup);
+
+    // //close popup
+    // $(btnPopup).click(function () {
+    //   $(popup).toggle();
+    //   $(popupOverlay).toggle();
+    // });
+
+    // $('.search__action').click(search);
+    //
+    // function getCoords(elem) {
+    //   var box = elem.getBoundingClientRect();
+    //
+    //   return {
+    //     top: box.top + pageYOffset,
+    //     left: box.left + pageXOffset
+    //   };
+    //
+    // }
+    // getCoords();
+
+    // function mouseShowHandler(e) {
+    //   e = e || window.event;
+    //   if (e.pageX == null && e.clientX != null) {
+    //     var html = document.documentElement;
+    //     var body = document.body;
+    //     e.pageX = e.clientX + (html && html.scrollLeft || body && body.scrollLeft || 0) - (html.clientLeft || 0);
+    //     e.pageY = e.clientY + (html && html.scrollTop || body && body.scrollTop || 0) - (html.clientTop || 0);
+    //   }
+    //   document.getElementById('mouseX').value = e.pageX;
+    //   document.getElementById('mouseY').value = e.pageY;
+    // }
+
+  //   mouseShowHandler();
+  //
+  //     var mouseCoords = {
+  //
+  //     x: 0,
+  //     y: 0,
+  //
+  //     set: function (e) {
+  //
+  //       if (e.pageX && e.pageY) {
+  //
+  //         this.x = e.pageX;
+  //         this.y = e.pageY;
+  //
+  //       } else if (e.clientX && e.clientY) {
+  //
+  //         this.x = e.clientX+(document.documentElement.scrollLeft || document.body.scrollLeft) - document.documentElement.clientLeft;
+  //         this.y = e.clientY+(document.documentElement.scrollTop || document.body.scrollTop) - document.documentElement.clientTop;
+  //
+  //       } else {
+  //
+  //         this.x = 0;
+  //         this.y = 0;
+  //
+  //       }
+  //
+  //     }
+  //
+  // };
+
+    // document.onmousemove = function (e) {
+    //
+    //   e = e || window.event;
+    //   MouseCoords.set(e);
+    //
+    // };
+
+    // $('body').mousedown({a:12, b:"abc"}, function(eventObject){
+    //   var externalData = "a=" + eventObject.data.a + ", b=" + eventObject.data.b;
+    //   alert('Был перемещен курсор мыши. '+
+    //     'В обработчик этого события переданы данные: ' + externalData );
+    // });
 
   }());
 });
